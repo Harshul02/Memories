0ac4026dea2d855f71af8cacc502d3ef2f8f9ca9
Harshul Jain <harshulgangwal02@gmail.com>
2023-09-24 17:29:20 +0530

    Added Route

--- server/index.js
+++ server/index.js
@@ -6,13 +6,15 @@ import 'dotenv/config';
 
 const app = express();
 
+import postRoutes from './routes/posts.js';
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
-// console.log(CONNECTION_URL);
+
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,10 @@
 import express from 'express';
 
-import { getPosts, createPost} from '../controllers/posts.js';
+import { getPosts} from '../controllers/posts.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
-router.post('/', createPost);
+// router.post('/', createPost);
+
+export default router;
