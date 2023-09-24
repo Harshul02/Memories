27a7a14b2f7c4e72fb418ac959c2e38e7a219aa4
Harshul Jain <harshulgangwal02@gmail.com>
2023-09-24 18:23:24 +0530

    Initial

--- server/index.js
+++ server/index.js
@@ -1,22 +1,30 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import 'dotenv/config';
-
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const multer = require("multer");
+const router = express.Router();
+const path = require("path");
 
-import postRoutes from './routes/posts.js';
+dotenv.config();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
-app.use('/posts', postRoutes);
+// console.log(process.env.MONGO_URL);
+async function connectToMongoDB() {
+    try {
+      await mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+    }
+  }
+  
+  connectToMongoDB();
 
-const CONNECTION_URL = process.env.CONNECTION_URL;
-
-const PORT = process.env.PORT|| 5000;
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+app.listen(5000, () => {
+    console.log("Backend server is running!");
+  });
