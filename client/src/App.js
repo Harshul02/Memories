a9107d442bc687d591ad80f97b6364c7c5823454
Harshul Jain <harshulgangwal02@gmail.com>
2023-09-24 20:18:54 +0530

    Added Dependencies and added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
-import React from "react";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import { Navigate } from "react-router-dom";
 
 function App() {
+  const { user } = useContext(AuthContext);
   return (
-    <div>
-      App
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={user ? <Home /> : <Register />} />
+          {/* {user ? <Home /> : <Register />} */}
+        {/* </Route> */}
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        {/* {user ? <Navigate to="/" /> : <Login />}</Route> */}
+        <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+          {/* {user ? <Navigate to="/" /> : <Register />} */}
+        {/* </Route> */}
+        <Route path="/profile/:username" element={<Profile />} />
+          {/* <Profile /> */}
+        {/* </Route> */}
+      </Routes>
+    </Router>
   );
 }
 
